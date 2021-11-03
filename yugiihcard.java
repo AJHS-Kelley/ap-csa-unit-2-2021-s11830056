1cdb0b85540e7a947f9bb8de6fa776e8f5c2d993
Johann B <s11830056@students.duvalschools.org>
2021-11-03 09:41:14 -0400

    v1.0

---
+++ yugiihcard.java
@@ -0,0 +1,22 @@
+// YuGiOh Card classes Revised, Johann Burke, v1.0
+
+public class yugiohcard {
+    private int attack;
+    private int defense;
+    private String cardName;
+    private String cardType;
+
+
+
+
+
+public yuGiOhcard() {
+    this.attack = 0
+    this.defense = 0
+    this.cardName = "";
+    this.cardType = "";
+
+
+}
+
+}
