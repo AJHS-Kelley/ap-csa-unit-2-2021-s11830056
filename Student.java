65cb139b7a5d294c98b6944ece8ac0413ccd16cc
Johann B <s11830056@students.duvalschools.org>
2021-11-03 09:10:55 -0400

    v0.5
    add public class Student

---
+++ Student.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+// Creating Classes and methods v0.5
+
+public class Student {
+    private int studentID;
+    private int gradeLevel;
+    private double gradePointAvg;
+    private String studentName;
+
+
+
+
+    
+
+
+
+
+    
+    
+}
